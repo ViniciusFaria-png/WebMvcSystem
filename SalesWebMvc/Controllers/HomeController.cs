f3030f5f31886df032223245d59daa21fdb16a94
Vinicius Faria <vinicius.f4ria@gmail.com>
2024-06-18 15:55:43 -0300

    First Controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -13,7 +13,8 @@ namespace SalesWebMvc.Controllers {
         }
 
         public IActionResult About() {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
+            ViewData["aluno"] = "Vinicius Faria";
 
             return View();
         }
